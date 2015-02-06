559fa76301a5dedad1ba7d2aea267c89206f070d
GrilloPress <grillopress@gmail.com>
2015-02-06 00:30:04 +0000

    update user controller

--- muskox/app/controllers/users_controller.rb
+++ muskox/app/controllers/users_controller.rb
@@ -12,6 +12,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
   def create
     @user = User.new(user_params)
     
@@ -20,6 +24,23 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    
+    redirect_to users_path
   end
   
   private
